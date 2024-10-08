6b7f9dbe3665afecb421642ec23b27b00c88fb98
Samuel Gracia <macedosam55@gmail.com>
2024-10-08 17:37:30 -0300

    Update regraOrgao.
    Atribui a todos os itens suas respectivas Procuradorias.

--- src/regraOrgao.py
+++ src/regraOrgao.py
@@ -14,71 +14,71 @@ regra["9"] = "Militar Estadual"
 # Tribunal Regional Federal da 1ª Região (AC, AM, AP, BA, DF, GO, MA, MT, PA, PI, RO, RR e TO)
 regra["401"] = ["TRF1", "PRU1"]
 # Tribunal Regional Federal da 2ª Região (ES e RJ)
-regra["402"] = ["TRF2", "Outra PR"]
+regra["402"] = ["TRF2", "PRU2"]
 # Tribunal Regional Federal da 3ª Região (MS e SP)
-regra["403"] = ["TRF3", "Outra PR"]
+regra["403"] = ["TRF3", "PRU3"]
 # Tribunal Regional Federal da 4ª Região (PR, RS e SC)
-regra["404"] = ["TRF4", "Outra PR"]
+regra["404"] = ["TRF4", "PRU4"]
 # Tribunal Regional Federal da 5ª Região (AL, CE, PB, PE, RN e SE)
-regra["405"] = ["TRF5", "Outra PR"]
+regra["405"] = ["TRF5", "PRU5"]
 # Tribunal Regional Federal da 6ª Região (MG)
-regra["406"] = ["TRF6", "Outra PR"]
+regra["406"] = ["TRF6", "PRU6"]
 
 # Tribunais Regionais do Trabalho (5):
 # "Tribunal Regional do Trabalho da 1ª Região (TRT1) (RJ)"
-regra["501"] = ["TRT1", "Outra PR"]
+regra["501"] = ["TRT1", "PRU2"]
 # "Tribunal Regional do Trabalho da 2ª Região (TRT2) (SP / Grande São Paulo e Baixada Santista)"
-regra["502"] = ["TRT2", "Outra PR"]
+regra["502"] = ["TRT2", "PRU3"]
 # "Tribunal Regional do Trabalho da 3ª Região (TRT3) (MG)"
-regra["503"] = ["TRT3", "Outra PR"]
+regra["503"] = ["TRT3", "PRU6"]
 # "Tribunal Regional do Trabalho da 4ª Região (TRT4) (RS)"
-regra["504"] = ["TRT4", "Outra PR"]
+regra["504"] = ["TRT4", "PRU4"]
 # "Tribunal Regional do Trabalho da 5ª Região (TRT5) (BA)"  PRU1
 regra["505"] = ["TRT5", "PRU1"]
 # "Tribunal Regional do Trabalho da 6ª Região (TRT6) (PE)"
-regra["506"] = ["TRT6", "Outra PR"]
+regra["506"] = ["TRT6", "PRU5"]
 # "Tribunal Regional do Trabalho da 7ª Região (TRT7) (CE)"
-regra["507"] = ["TRT7", "Outra PR"]
+regra["507"] = ["TRT7", "PRU5"]
 # "Tribunal Regional do Trabalho da 8ª Região (TRT8) (AP e PA)" PRU1
 regra["508"] = ["TRT8", "PRU1"]
 # "Tribunal Regional do Trabalho da 9ª Região (TRT9) (PR)"
-regra["509"] = ["TRT9", "Outra PR"]
+regra["509"] = ["TRT9", "PRU4"]
 # "Tribunal Regional do Trabalho da 10ª Região (TRT10) (DF e TO)"   PRU1
 regra["510"] = ["TRT10", "PRU1"]
 # "Tribunal Regional do Trabalho da 11ª Região (TRT11) (AM e RR)"   PRU1
 regra["511"] = ["TRT11", "PRU1"]
 # "Tribunal Regional do Trabalho da 12ª Região (TRT12) (SC)"
-regra["512"] = ["TRT12", "Outra PR"]
+regra["512"] = ["TRT12", "PRU4"]
 # "Tribunal Regional do Trabalho da 13ª Região (TRT13) (PB)"
-regra["513"] = ["TRT13", "Outra PR"]
+regra["513"] = ["TRT13", "PRU5"]
 # "Tribunal Regional do Trabalho da 14ª Região (TRT14) (AC e RO)"   PRU1
 regra["514"] = ["TRT14", "PRU1"]
 # "Tribunal Regional do Trabalho da 15ª Região (TRT15) (SP / Interior e Litoral Norte e Sul)"
-regra["515"] = ["TRT15", "Outra PR"]
+regra["515"] = ["TRT15", "PRU3"]
 # "Tribunal Regional do Trabalho da 16ª Região (TRT16) (MA)"    PRU1
 regra["516"] = ["TRT16", "PRU1"]
 # "Tribunal Regional do Trabalho da 17ª Região (TRT17) (ES)"
-regra["517"] = ["TRT17", "Outra PR"]
+regra["517"] = ["TRT17", "PRU2"]
 # "Tribunal Regional do Trabalho da 18ª Região (TRT18) (GO)"    PRU1
 regra["518"] = ["TRT18", "PRU1"]
 # "Tribunal Regional do Trabalho da 19ª Região (TRT19) (AL)"
-regra["519"] = ["TRT19", "Outra PR"]
+regra["519"] = ["TRT19", "PRU5"]
 # "Tribunal Regional do Trabalho da 20ª Região (TRT20) (SE)"
-regra["520"] = ["TRT20", "Outra PR"]
+regra["520"] = ["TRT20", "PRU5"]
 # "Tribunal Regional do Trabalho da 21ª Região (TRT21) (RN)"
-regra["521"] = ["TRT21", "Outra PR"]
+regra["521"] = ["TRT21", "PRU5"]
 # "Tribunal Regional do Trabalho da 22ª Região (TRT22) (PI)"    PRU1
 regra["522"] = ["TRT22", "PRU1"]
 # "Tribunal Regional do Trabalho da 23ª Região (TRT23) (MT)"    PRU1
 regra["523"] = ["TRT23", "PRU1"]
 # "Tribunal Regional do Trabalho da 24ª Região (TRT24) (MS)"
-regra["524"] = ["TRT24", "Outra PR"]
+regra["524"] = ["TRT24", "PRU3"]
 
 # Justiça Regional Eleitoral (6):
 # Tribunal Regional Eleitoral do Acre   PRU1
 regra["601"] = ["TRE-AC", "PRU1"]
 # Tribunal Regional Eleitoral de Alagoas
-regra["602"] = ["TRE-AL", "Outra PR"]
+regra["602"] = ["TRE-AL", "PRU5"]
 # Tribunal Regional Eleitoral do Amapá  PRU1
 regra["603"] = ["TRE-AP", "PRU1"]
 # Tribunal Regional Eleitoral do Amazonas   PRU1
@@ -86,11 +86,11 @@ regra["604"] = ["TRE-AM", "PRU1"]
 # Tribunal Regional Eleitoral da Bahia  PRU1
 regra["605"] = ["TRE-BA", "PRU1"]
 # Tribunal Regional Eleitoral do Ceará
-regra["606"] = ["TRE-CE", "Outra PR"]
+regra["606"] = ["TRE-CE", "PRU5"]
 # Tribunal Regional Eleitoral do Distrito Federal   PRU1
 regra["607"] = ["TRE-DF", "PRU1"]
 # Tribunal Regional Eleitoral do Espírito Santo
-regra["608"] = ["TRE-ES", "Outra PR"]
+regra["608"] = ["TRE-ES", "PRU2"]
 # Tribunal Regional Eleitoral de Goiás  PRU1
 regra["609"] = ["TRE-GO", "PRU1"]
 # Tribunal Regional Eleitoral do Maranhão   PRU1
@@ -98,69 +98,69 @@ regra["610"] = ["TRE-MA", "PRU1"]
 # Tribunal Regional Eleitoral do Mato Grosso    PRU1
 regra["611"] = ["TRE-MT", "PRU1"]
 # Tribunal Regional Eleitoral do Mato Grosso do Sul
-regra["612"] = ["TRE-MS", "Outra PR"]
+regra["612"] = ["TRE-MS", "PRU3"]
 # Tribunal Regional Eleitoral de Minas Gerais
-regra["613"] = ["TRE-MG", "Outra PR"]
+regra["613"] = ["TRE-MG", "PRU6"]
 # Tribunal Regional Eleitoral do Pará   PRU1
 regra["614"] = ["TRE-PA", "PRU1"]
 # Tribunal Regional Eleitoral da Paraíba
-regra["615"] = ["TRE-PB", "Outra PR"]
+regra["615"] = ["TRE-PB", "PRU5"]
 # Tribunal Regional Eleitoral do Paraná
-regra["616"] = ["TRE-PR", "Outra PR"]
+regra["616"] = ["TRE-PR", "PRU4"]
 # Tribunal Regional Eleitoral de Pernambuco
-regra["617"] = ["TRE-PE", "Outra PR"]
+regra["617"] = ["TRE-PE", "PRU5"]
 # Tribunal Regional Eleitoral do Piauí  PRU1
 regra["618"] = ["TRE-PI", "PRU1"]
 # Tribunal Regional Eleitoral do Rio de Janeiro
-regra["619"] = ["TRE-RJ", "Outra PR"]
+regra["619"] = ["TRE-RJ", "PRU2"]
 # Tribunal Regional Eleitoral do Rio Grande do Norte
-regra["620"] = ["TRE-RN", "Outra PR"]
+regra["620"] = ["TRE-RN", "PRU5"]
 # Tribunal Regional Eleitoral do Rio Grande do Sul
-regra["621"] = ["TRE-RS", "Outra PR"]
+regra["621"] = ["TRE-RS", "PRU4"]
 # Tribunal Regional Eleitoral de Rondônia   PRU1
 regra["622"] = ["TRE-RO", "PRU1"]
 # Tribunal Regional Eleitoral de Roraima    PRU1
 regra["623"] = ["TRE-RR", "PRU1"]
 # Tribunal Regional Eleitoral de Santa Catarina
-regra["624"] = ["TRE-SC", "Outra PR"]
+regra["624"] = ["TRE-SC", "PRU4"]
 # Tribunal Regional Eleitoral de São Paulo
-regra["625"] = ["TRE-SP", "Outra PR"]
+regra["625"] = ["TRE-SP", "PRU3"]
 # Tribunal Regional Eleitoral de Sergipe
-regra["626"] = ["TRE-SE", "Outra PR"]
+regra["626"] = ["TRE-SE", "PRU5"]
 # Tribunal Regional Eleitoral de Tocantins  PRU1
 regra["627"] = ["TRE-TO", "PRU1"]
 
 # Justiça Regional Eleitoral (7):
-regra["701"] = ["1ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["702"] = ["2ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["703"] = ["3ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["704"] = ["4ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["705"] = ["5ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["706"] = ["6ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["707"] = ["7ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["708"] = ["8ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["709"] = ["9ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["710"] = ["10ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["711"] = ["11ª Circunscrição Judiciária Militar", "Outra PR"]
-regra["712"] = ["12ª Circunscrição Judiciária Militar", "Outra PR"]
+regra["701"] = ["1ª Circunscrição Judiciária Militar", "Outro"]
+regra["702"] = ["2ª Circunscrição Judiciária Militar", "Outro"]
+regra["703"] = ["3ª Circunscrição Judiciária Militar", "Outro"]
+regra["704"] = ["4ª Circunscrição Judiciária Militar", "Outro"]
+regra["705"] = ["5ª Circunscrição Judiciária Militar", "Outro"]
+regra["706"] = ["6ª Circunscrição Judiciária Militar", "Outro"]
+regra["707"] = ["7ª Circunscrição Judiciária Militar", "Outro"]
+regra["708"] = ["8ª Circunscrição Judiciária Militar", "Outro"]
+regra["709"] = ["9ª Circunscrição Judiciária Militar", "Outro"]
+regra["710"] = ["10ª Circunscrição Judiciária Militar", "Outro"]
+regra["711"] = ["11ª Circunscrição Judiciária Militar", "Outro"]
+regra["712"] = ["12ª Circunscrição Judiciária Militar", "Outro"]
 
 # Tribunal de Justiça (8):
 # Tribunal de Justiça do Acre (TJAC)    PRU1
 regra["801"] = ["TJAC", "PRU1"]
 # Tribunal de Justiça de Alagoas (TJAL)
-regra["802"] = ["TJAL", "Outra PR"]
+regra["802"] = ["TJAL", "PRU5"]
 # Tribunal de Justiça do Amapá (TJAP)   PRU1
 regra["803"] = ["TJAP", "PRU1"]
 # Tribunal de Justiça do Amazonas (TJAM)    PRU1
 regra["804"] = ["TJAM", "PRU1"]
 # Tribunal de Justiça da Bahia (TJBA)   PRU1
-regra["805"] = ["TJBA", "Outra PR"]
+regra["805"] = ["TJBA", "PRU1"]
 # Tribunal de Justiça do Ceará (TJCE)
-regra["806"] = ["TJCE", "Outra PR"]
+regra["806"] = ["TJCE", "PRU5"]
 # Tribunal de Justiça do Distrito Federal e Territórios (TJDFT) PRU1
 regra["807"] = ["TJDFT", "PRU1"]
 # Tribunal de Justiça do Espírito Santo (TJES)
-regra["808"] = ["TJES", "Outra PR"]
+regra["808"] = ["TJES", "PRU2"]
 # Tribunal de Justiça de Goiás (TJGO)   PRU1
 regra["809"] = ["TJGO", "PRU1"]
 # Tribunal de Justiça do Maranhão (TJMA)    PRU1
@@ -168,42 +168,42 @@ regra["810"] = ["TJMA", "PRU1"]
 # Tribunal de Justiça de Mato Grosso (TJMT) PRU1
 regra["811"] = ["TJMT", "PRU1"]
 # Tribunal de Justiça de Mato Grosso do Sul (TJMS)
-regra["812"] = ["TJMS", "Outra PR"]
+regra["812"] = ["TJMS", "PRU3"]
 # Tribunal de Justiça de Minas Gerais (TJMG)
-regra["813"] = ["TJMG", "Outra PR"]
+regra["813"] = ["TJMG", "PRU6"]
 # Tribunal de Justiça do Pará (TJPA)    PRU1
 regra["814"] = ["TJPA", "PRU1"]
 # Tribunal de Justiça da Paraíba (TJPB)
-regra["815"] = ["TJPB", "Outra PR"]
+regra["815"] = ["TJPB", "PRU5"]
 # Tribunal de Justiça do Paraná (TJPR)
-regra["816"] = ["TJPR", "Outra PR"]
+regra["816"] = ["TJPR", "PRU4"]
 # Tribunal de Justiça de Pernambuco (TJPE)
-regra["817"] = ["TJPE", "Outra PR"]
+regra["817"] = ["TJPE", "PRU5"]
 # Tribunal de Justiça do Piauí (TJPI)   PRU1
 regra["818"] = ["TJPI", "PRU1"]
 # Tribunal de Justiça do Rio de Janeiro (TJRJ)
-regra["819"] = ["TJRJ", "Outra PR"]
+regra["819"] = ["TJRJ", "PRU2"]
 # Tribunal de Justiça do Rio Grande do Norte (TJRN)
-regra["820"] = ["TJRN", "Outra PR"]
+regra["820"] = ["TJRN", "PRU5"]
 # Tribunal de Justiça do Rio Grande do Sul (TJRS)
-regra["821"] = ["TJRS", "Outra PR"]
+regra["821"] = ["TJRS", "PRU4"]
 # Tribunal de Justiça de Rondônia (TJRO)    PRU1
 regra["822"] = ["TJRO", "PRU1"]
 # Tribunal de Justiça de Roraima (TJRR) PRU1
 regra["823"] = ["TJRR", "PRU1"]
 # Tribunal de Justiça de Santa Catarina (TJSC)
-regra["824"] = ["TJSC", "Outra PR"]
+regra["824"] = ["TJSC", "PRU4"]
 # Tribunal de Justiça de São Paulo (TJSP)
-regra["825"] = ["TJSP", "Outra PR"]
+regra["825"] = ["TJSP", "PRU3"]
 # Tribunal de Justiça de Sergipe (TJSE)
-regra["826"] = ["TJSE", "Outra PR"]
+regra["826"] = ["TJSE", "PRU5"]
 # Tribunal de Justiça do Tocantins (TJTO)   PRU1
 regra["827"] = ["TJTO", "PRU1"]
 
 # Tribunal Militar Estadual (9):
-regra["913"] = ["Tribunal de Justiça Militar do Estado de Minas Gerais", "Outra PR"]
-regra["921"] = ["Tribunal de Justiça Militar do Estado do Rio Grande do Sul", "Outra PR"]
-regra["926"] = ["Tribunal de Justiça Militar do Estado de São Paulo", "Outra PR"]
+regra["913"] = ["Tribunal de Justiça Militar do Estado de Minas Gerais", "PRU6"]
+regra["921"] = ["Tribunal de Justiça Militar do Estado do Rio Grande do Sul", "PRU4"]
+regra["926"] = ["Tribunal de Justiça Militar do Estado de São Paulo", "PRU3"]
 
 
 # Código para testes
